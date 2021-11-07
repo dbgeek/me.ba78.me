8dcd8be4f125b4507e0606314bab1f507184d2a7
Björn Ahl <bnal@ba78.me>
2021-11-07 14:52:58 +0100

    feature: adding nav bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,40 @@
-import 'tailwindcss/tailwind.css'
+import Link from 'next/link';
+import 'tailwindcss/tailwind.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <nav clasNames="bg-gray-100">
+        <div className="max-w-7xl mx-auto px-4">
+          <div className="flex justify-between">
+            <div className="flex space-x-4">
+              <div>
+                <Link href="/about">
+                  <a className="flex items-center py-5 px-2 text-gray-700 hover:text-gray-900">
+                    <svg className="h-6 w-6 mr-1 text-blue-400" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
+                    </svg>
+                    <span className="font-bold">about</span>
+                  </a>
+                </Link>
+              </div>
+              <div className="flex items-center space-x-1">
+                <Link href="/dogs">
+                  <a className="py-5 px-3 text-gray-700 hover:text-gray-900">dogs</a>
+                </Link>
+                <Link href="/blog">
+                  <a className="py-5 px-3 text-gray-700 hover:text-gray-900">blog</a>
+                </Link>
+              </div>
+            </div>
+            <div>
+            </div>
+          </div>
+        </div>
+      </nav>
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
---
+++ pages/dogs/index.js
@@ -0,0 +1,7 @@
+export default function Blog() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1 className="font-mono text-5xl text-purple-700 text-opacity-25 hover:text-red-500 ...">Dogs</h1>
+    </div>
+  )
+}
