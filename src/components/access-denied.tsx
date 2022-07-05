2ee9a586bb0389af658270372e5f007db9541edd
Björn Ahl <bnal@ba78.me>
2022-07-05 22:58:22 +0200

    build: fixing lint error

--- src/components/access-denied.tsx
+++ src/components/access-denied.tsx
@@ -1,15 +1,17 @@
-import { signIn } from 'next-auth/react'
+import Link from 'next/link';
+import { signIn } from 'next-auth/react';
+
 
 export default function AccessDenied () {
   return (
     <>
       <h1>Access Denied</h1>
       <p>
-        <a href="/api/auth/signin"
+        <Link href="/api/auth/signin"
            onClick={(e) => {
            e.preventDefault()
            signIn()
-        }}>You must be signed in to view this page</a>
+        }}>You must be signed in to view this page</Link>
       </p>
     </>
   )
