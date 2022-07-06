d7d96fde13999f68deb4b76dfb3d186cba2a1d22
Björn Ahl <bnal@ba78.me>
2022-07-06 15:18:16 +0200

    debug: print github_id

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,13 +1,18 @@
 import { GetServerSideProps } from 'next'
+import { NextPage } from "next";
 
 import Header from "../../components/header";
 
-export default function Blog({data}) {
+interface Props {
+  data: string;
+}
+
+const Blog: NextPage<Props> = (props) => {
   return (
     <>
       <Header />
       <div className="flex flex-col items-center justify-center min-h-screen py-2">
-        <h1 className="font-mono text-5xl text-purple-700 text-opacity-25 hover:text-red-500 ...">{data}</h1>
+        <h1 className="font-mono text-5xl text-purple-700 text-opacity-25 hover:text-red-500 ...">{props.data}</h1>
       </div>
     </>
   )
@@ -16,3 +21,5 @@ export default function Blog({data}) {
 export const getServerSideProps: GetServerSideProps = async (context) => {
   return { props: { data: process.env.GITHUB_ID } }
 }
+
+export default Blog;
