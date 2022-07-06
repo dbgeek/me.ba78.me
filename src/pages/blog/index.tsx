23d38163f6001ff8a0e3fa12b4675b719ca0c771
Björn Ahl <bnal@ba78.me>
2022-07-06 15:07:00 +0200

    debug: is GITHUB_ID set

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,12 +1,18 @@
+import { GetServerSideProps } from 'next'
+
 import Header from "../../components/header";
 
-export default function Blog() {
+export default function Blog({data}) {
   return (
     <>
       <Header />
       <div className="flex flex-col items-center justify-center min-h-screen py-2">
-        <h1 className="font-mono text-5xl text-purple-700 text-opacity-25 hover:text-red-500 ...">Blog</h1>
+        <h1 className="font-mono text-5xl text-purple-700 text-opacity-25 hover:text-red-500 ...">{data}</h1>
       </div>
     </>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  return { props: { data: process.env.GITHUB_ID } }
 }
